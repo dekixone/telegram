ed2eb1da0040a99ff8cccdf091dd5a782c4ace6c
dekixone <32235156+dekixone@users.noreply.github.com>
2017-09-24 15:52:25 +0700

    Update LatihanBot.php

--- LatihanBot.php
+++ LatihanBot.php
@@ -75,8 +75,7 @@ function create_response($text, $message)
        // jika ada pesan /id, bot akan membalas dengan menyebutkan idnya user
         case '/90170038':
         case '/90170038'.$usernamebot : //dipakai jika di grup yang haru ditambahkan @usernamebot
-            $hasil = "Nama : $namauser, ID kamu adalah $fromid";
-            $hasil .= 'NIK :90170038 Unit Kerja : SDI Telkom Akses Bengkulu';
+            $hasil = 'Nama : Deki Firmansyah,S.Kom NIK :90170038 Unit Kerja : SDI Telkom Akses Bengkulu';
             break;
       
         // jika ada pesan /id, bot akan membalas dengan menyebutkan idnya user
